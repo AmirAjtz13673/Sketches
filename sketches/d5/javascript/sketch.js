9e5d6b3b1de4a30ed8c85518407be3433c815722
AmirAjtz13673 <127853667+AmirAjtz13673@users.noreply.github.com>
2023-03-29 19:20:51 -0400

    Add files via upload

---
+++ sketches/d5/javascript/sketch.js
@@ -0,0 +1,33 @@
+let myFont;
+let blockWidth = 30;  
+let blockHeight = 100;
+
+
+
+function setup() {
+  createCanvas(1000, 1000);
+  background(0);
+}
+
+function draw() {
+
+  stroke(255);
+  strokeWeight(4);
+  
+
+  
+
+  for (let i = 0; i < 0.1; i++) {
+
+    let startX = random(100, 900);
+    let startY = random(100, 900);
+    let endX = random(100, 900);
+    let endY = random(100, 900);
+    
+
+    line(startX, startY, endX, endY);
+  }
+
+  strokeWeight(8);
+  line(500, 100, 500, 900);
+}
